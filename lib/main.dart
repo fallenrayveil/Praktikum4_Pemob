5da8f053746577666f585400a92e601adfb80f1d
fallenrayveil <rahmawanas6@gmail.com>
2024-03-22 05:57:11 +0700

    Add recipe detail screen and update recipe card layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'package:recipeapp/recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -58,7 +59,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index) {
-            return buildRecipeCard(Recipe.samples[index]);
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
           },
         ),
       ),
@@ -68,11 +81,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
 Widget buildRecipeCard(Recipe recipe) {
   return Card(
-    child: Column(
-      children: [
-        Image(image: AssetImage(recipe.imageUrl)),
-        Text(recipe.label),
-      ],
+    elevation: 2.0,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10.0),
     ),
-  );
-}
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Image(image: AssetImage(recipe.imageUrl)),
+          const SizedBox(
+            height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
